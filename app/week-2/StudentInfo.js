12546742bae1df5065dc7b2ce57dc2d50d7e2c83
Dylan Khuu <khuudylan@gmail.com>
2026-01-23 15:03:53 -0700

    Created StudenInfo.js

--- app/week-2/StudentInfo.js
+++ app/week-2/StudentInfo.js
@@ -1,7 +1,10 @@
+import Link from "next/link"
+
 export default function StudentInfo() {
   return (
     <div>
-      
+      <p>Dylan Khuu</p>
+      <Link href="https://github.com/RealTestingp/cprg306-assignments">GitHub Repository Link</Link>
     </div>
   )
 }

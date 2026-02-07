d6b056ce1448bae712892bed2cc0272d90f6d135
Dylan Khuu <khuudylan@gmail.com>
2026-02-06 18:23:17 -0700

    Shopping List Grouped by Categories

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -4,7 +4,7 @@ import items from "./items"
 const Page = () => {
   return (
     <main className="flex flex-col items-center">
-      <h1 className="text-2xl font-bold py-4 w-full max-w-lg">Shopping List</h1>
+      <h1 className="text-2xl font-bold py-4 w-full max-w-lg">Shopping List Grouped by Categories</h1>
       <ItemList items={items}/>
     </main>
   )

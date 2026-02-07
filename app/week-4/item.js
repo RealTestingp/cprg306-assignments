758f3f33c16a1e04e13a3578fcf4daf466412ea9
Dylan Khuu <khuudylan@gmail.com>
2026-02-06 17:51:19 -0700

    Removed redundant category within box

--- app/week-4/item.js
+++ app/week-4/item.js
@@ -4,9 +4,9 @@ const Item = ({ name, quantity, category }) => {
       <div className="flex justify-between items-center p-4">
         <div>
           <h2 className="text-lg text-black font-semibold capitalize">{name}</h2>
+          {/* <p className="text-sm text-gray-500 capitalize">{category}</p> */}
           <p className="text-sm text-gray-500">Quantity: {quantity}</p>
         </div>
-        <span className="px-2 py-1 bg-blue-100 text-blue-800 text-xs font-medium rounded">{category}</span>
       </div>
     </ul>
   );

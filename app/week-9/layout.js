da2b03cb85b241a2b3bf4c0be7c637f73aee0fda
Dylan Khuu <khuudylan@gmail.com>
2026-03-20 08:58:06 -0600

    layout folder fix

--- app/week-9/layout.js
+++
@@ -1,5 +0,0 @@
-import { AuthContextProvider } from "./contexts/AuthContext.js";
-
-export default function Layout({ children }) {
-  return <AuthContextProvider>{children}</AuthContextProvider>;
-}
